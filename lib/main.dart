feb0dd14e650c6b2ff59f7f1f4f91fa12b9be4b2
chobokoanemt <chobokoanemt@gmail.com>
2020-03-24 09:40:19 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_place_picker/google_maps_place_picker.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // Light Theme
+  final ThemeData lightTheme = ThemeData.light().copyWith(
+    // Background color of the FloatingCard
+    cardColor: Colors.white,
+    buttonTheme: ButtonThemeData(
+      // Select here's button color
+      buttonColor: Colors.black,
+      textTheme: ButtonTextTheme.primary,
+    ),
+  );
+
+  // Dark Theme
+  final ThemeData darkTheme = ThemeData.dark().copyWith(
+    // Background color of the FloatingCard
+    cardColor: Colors.grey,
+    buttonTheme: ButtonThemeData(
+      // Select here's button color
+      buttonColor: Colors.yellow,
+      textTheme: ButtonTextTheme.primary,
+    ),
+  );
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google Map Place Picker Demo',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.dark,
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  static final kInitialPosition = LatLng(-33.8567844, 151.213108);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PickResult selectedPlace;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Google Map Place Picer Demo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("Load Google Map"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return PlacePicker(
+                          apiKey:"AIzaSyDUKEDHug3fKceIrztjZBXImtXmEukqNfo",
+                          initialPosition: HomePage.kInitialPosition,
+                          useCurrentLocation: true,
+                          usePlaceDetailSearch: true,
+                          onPlacePicked: (result) {
+                            selectedPlace = result;
+                            Navigator.of(context).pop();
+                            setState(() {});
+                          },
+                          autocompleteLanguage: "ko",
+
+                          // },
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+              selectedPlace == null ? Container() : Text(selectedPlace.formattedAddress ?? ""),
+            ],
+          ),
+        ));
+  }
+}
